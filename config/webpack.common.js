97bcd4fe125b7a98c765d6f7c4c24a32cbb21e52
Evgeny Glukhov <evglukhov@luxoft.com>
2018-04-10 08:29:17 +0300

    add aot folder path

--- config/webpack.common.js
+++ config/webpack.common.js
@@ -14,7 +14,10 @@ module.exports = {
     },
 
     resolve: {
-        extensions: ['.ts', '.js']    
+        extensions: ['.ts', '.js'],
+        alias: {
+            '@aot': helpers.root('aot')
+        }    
     },
 
     module: {
